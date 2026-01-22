0e1f66b7bb787a9f81533054c68b00483cbef362
stefanvill <Stefan@villegas.se>
2026-01-22 14:49:49 +0100

    Changed Layout, added html, controller and product service

---
+++ src/main/java/se/iths/stefan/ovningspringboot/controller/ProductController.java
@@ -0,0 +1,31 @@
+package se.iths.stefan.ovningspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import se.iths.stefan.ovningspringboot.model.Product;
+import se.iths.stefan.ovningspringboot.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/inventory")
+    public String getProducts(Model model) {
+        List<Product> productList = productService.productList();
+        model.addAttribute("products", productList);
+        return "product";
+    }
+
+}
